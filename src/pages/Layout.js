dc11103c58bfd98381bf829488e4c21000e40bf2
raghav-pant17 <79854595+raghav-pant17@users.noreply.github.com>
2023-10-12 12:19:46 +0530

    initial commit

---
+++ src/pages/Layout.js
@@ -0,0 +1,22 @@
+import { Outlet, Link } from "react-router-dom";
+import Appbar from "../components/Appbar";
+import { Stack } from "@mui/material";
+// import Ngrid from './grid'
+import "./Layout.css";
+
+const Layout = () => {
+  return (
+    <div>
+      <Stack spacing={5}>
+        <div className="home-banner">
+          <Appbar />
+          <div className="newcontainer">
+            <Outlet />
+          </div>
+        </div>
+      </Stack>
+    </div>
+  );
+};
+
+export default Layout;
