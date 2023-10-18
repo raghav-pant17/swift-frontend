f65df21398c01d0e01ad01834ab6fda3d390f6e2
Daksh Bhatia <dakshbh@amdocs.com>
2023-10-18 10:09:56 +0530

    added components for select seats

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -7,18 +7,46 @@ import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
-const Appbar = () => {
-  return (
-     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static"  sx={{ bgcolor: "#002984" }}>
-        <Toolbar>
-          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/" style={{textDecoration:'none', color:'white' }}>SWIFT </Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  )
+
+const Appbar = (props) => {
+
+  console.log("props value :")
+    console.log(props)
+    
+  if(props.showData == null){
+    
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+       <AppBar position="static"  sx={{ bgcolor: "#002984" }}>
+       <Toolbar>
+           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+             <Link to="/" style={{textDecoration:'none', color:'white' }}>SWIFT</Link>
+           </Typography>
+         </Toolbar>
+       </AppBar>
+     </Box>
+   )
+  }
+
+  else{
+    var showData = props.showData
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+       <AppBar position="static"  sx={{ bgcolor: "#002984" }}>
+       <Toolbar>
+           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+             {showData ? showData.movie.name : ""}
+           </Typography><br/>
+           <Typography  component="div" sx={{ flexGrow: 1 }}>
+             {showData ? showData.theater.name : ""}: { showData ? showData.theater.address : ""}&nbsp;|&nbsp;{showData ? showData.time : ""} 
+           </Typography>
+         </Toolbar>
+       </AppBar>
+     </Box>
+    )
+  }
+
+  
 }
 
 export default Appbar;
--- src/pages/SelectSeats.js
+++ src/pages/SelectSeats.js
@@ -3,9 +3,12 @@ import { useParams } from "react-router-dom";
 import {useLocation} from 'react-router-dom';
 import Showcase from "../components/selectSeats/Showcase";
 import '../components/selectSeats/selectSeats.css'
-import { Stack } from "@mui/material";
-import Container from '@mui/material/Container';
+import { BottomNavigation, Stack } from "@mui/material";
 import Grid from '@mui/material/Grid';
+import SeatGrid from "../components/selectSeats/SeatGrid";
+import PriceLegend from "../components/selectSeats/PriceLegend";
+import SeatInfo from "../components/selectSeats/SeatInfo";
+import BottomNavigator from "../components/selectSeats/BottomNavigator";
 
 
 export default function SelectSeats(){
@@ -14,28 +17,37 @@ export default function SelectSeats(){
 
     //const [numSeats, setNumSeats] = useState(location.state.numSeats)
     const [numSeats, setNumSeats] = useState(3)
-    const [unavailableSeats, setUnavailableSeats] = useState([])
+    const [occupiedSeats, setOccupiedSeats] = useState([])
+    const [selectedSeats, setSelectedSeats] = useState([])
     const [prices, setPrices] = useState({})
+    const [show, setShow] = useState({})
 
     console.log(numSeats)
     return(
         <div className="SeatSelectionWindow">
-        <Grid container spacing={10}>
+        <Grid container spacing={5} paddingBottom={10}>
             
-            <Grid item xs={4}>
-                <Stack spacing={3} alignItems={"center"} direction="column">
+            <Grid item xs={4}  alignItems="center" direction="row" justifyContent="center" >
+                
+                <Stack spacing={3} alignItems={"center"} direction="column" marginLeft={8}>
                     <Showcase/>
+                    <PriceLegend/>
+                    <SeatInfo prices = {prices} selectedSeats = {selectedSeats}/>
                 </Stack>
             </Grid>
 
             <Grid item xs={8}>
-            <Stack className="Cinema" spacing={3} alignItems={"center"} direction="column">
-            
+            <Stack className="Cinema" spacing={8} alignItems={"center"} direction="column">
                     <div className="screen" />
-                
+                    <SeatGrid occupiedSeats={occupiedSeats} selectedSeats={selectedSeats} setSelectedSeats={setSelectedSeats} numSeats = {numSeats}/>
             </Stack>
             </Grid>
         </Grid>
+
+        {
+        (selectedSeats.length == numSeats) &&
+             <BottomNavigator/>
+        }
         </div>
         
     )
